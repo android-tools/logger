8559ccf455e7bd526fc9b173b6da3743c83763c5
busylee999 <demonlee999@gmail.com>
2015-10-14 11:55:50 +0300

    added missed Mime class

---
+++ loglib/src/main/java/com/busylee/file/Mime.java
@@ -0,0 +1,47 @@
+package com.busylee.file;
+
+import android.text.TextUtils;
+import android.webkit.MimeTypeMap;
+
+public class Mime {
+
+	/** Получение mime type из ссылки на файл */
+	public static String getMimeType(String url) {
+	    String type = null;
+	    String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+
+		/* to avoid empty file
+		 *	extension string when url contains #
+		 */
+		if (TextUtils.isEmpty(extension))
+			extension = getFileExtension(url);
+
+	    if (extension != null) {
+	        MimeTypeMap mime = MimeTypeMap.getSingleton();
+	        type = mime.getMimeTypeFromExtension(extension);
+	    }
+	    
+	    if(type == null)
+	    	type = getMimeFromLocalMap(extension);
+	    
+	    return type;
+	}
+
+	private static String getFileExtension(String path){
+		String ext;
+		if(path!= null && path.lastIndexOf(".") != -1){
+			int index=path.lastIndexOf(".");
+			ext=path.substring(index+1,path.length());
+
+		}else{
+			ext="";
+		}
+		return ext;
+	}
+
+	@Deprecated
+	private static String getMimeFromLocalMap(String extension) {
+		return null;
+	}
+
+}
